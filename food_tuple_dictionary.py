84998c3cd961010bdd728251a3f128c4be324b95
vaniroo <vaniroman10@gmail.com>
2016-08-16 21:46:24 -0700

    Add files via upload
    wip still need to finish code appetizer

---
+++ food_tuple_dictionary.py
@@ -0,0 +1,6 @@
+food_price_tuple = ('sushi', 7.50, 'burrito', 8.20, 'cheeseburger', 6.00, 'hot dog', 3.40, 'fried rice', 9.99)
+
+food_dictionary = {}
+
+# def convert_tuple_to_dictionary
+# 	for food
