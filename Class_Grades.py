5495c011c72625be58b8d9dfdfb990159d795893
vaniroo <vaniroman10@gmail.com>
2016-08-23 21:28:32 -0700

    Add files via upload

---
+++ Class_Grades.py
@@ -0,0 +1,4 @@
+#nMake a dictionary and have the key equal the letter, and the value be the list with the corresponding numbers
+#Make the function that calls the text file, and the match each row with the corresponding number/key, by iterating through the key values in our dictionary
+
+Grades_Number = {"A":range(90,101,1)}
